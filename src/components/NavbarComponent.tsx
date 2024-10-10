86dd0a73bfdc0d0262d22322d8555c68b232e829
Serhat Karaman <90270116+MasterofN0ne@users.noreply.github.com>
2024-10-11 02:16:53 +0300

    iframe fix#4

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function NavbarComponent() {
   const [scrollPosition, setScrollPosition] = useState(0);
