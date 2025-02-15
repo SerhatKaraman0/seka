da9c6d97db4356d273b136eb02df0150e2028cb0
Serhat Karaman <90270116+MasterofN0ne@users.noreply.github.com>
2025-02-15 21:08:46 +0300

    changes in settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
 export default defineConfig({
   server: {
     host: '0.0.0.0',
@@ -12,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/seka/",
+  base: "/",
 });
